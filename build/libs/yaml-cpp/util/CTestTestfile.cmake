b8f38daa6ec14712c0f18aca8d44ea50c60ef157
rinaz <garaevribaz@gmail.com>
2025-03-06 09:44:26 +0300

    build and cmake debug dirs is rm

--- build/libs/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Code/hse_classes/Game life/libs/yaml-cpp/util
-# Build directory: C:/Code/hse_classes/Game life/build/libs/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/libs/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Code/hse_classes/Game life/libs/yaml-cpp
-# Build directory: C:/Code/hse_classes/Game life/cmake-build-debug/libs/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
